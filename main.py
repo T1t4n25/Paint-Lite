e357784cec92bf148dd283a1a9a5286750daacd4
Zeyad Hemeda <shadodiss@gmail.com>
2023-12-25 21:25:17 +0200

    Refactor polygon drawing and line rendering functions

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from tkinter import *
+import math
 shape = 0
-pixel_size = 5
+pixel_size = 2
 connected_lines = True
+sides = 360
+
+def draw_polygon(event):
+    global click_num
+    global center_x, center_y
+    global sides
+
+    if click_num == 0:
+        center_x = event.x
+        center_y = event.y
+        click_num = 1
+    else:
+        radius = math.sqrt((event.x - center_x)**2 + (event.y - center_y)**2)
+        draw_regular_polygon(center_x, center_y, radius, sides)
+        click_num = 0
+
+def draw_regular_polygon(center_x, center_y, radius, sides):
+    angle_increment = 360 / sides  # Angle increment in degrees
+    current_angle = 0
+
+    x1, y1 = center_x + radius * math.cos(math.radians(current_angle)), center_y - radius * math.sin(math.radians(current_angle))
+
+    for _ in range(sides):
+        current_angle += angle_increment
+        x2 = center_x + radius * math.cos(math.radians(current_angle))
+        y2 = center_y - radius * math.sin(math.radians(current_angle))
+        ddaLine(x1, y1, x2, y2, pixel_size)
+        x1, y1 = x2, y2
+
 def draw_line(event):
     global click_num
     global x1, y1
@@ -27,13 +57,13 @@ def ddaLine(x1, y1, x2, y2, pixel_size):
     dx = x2 - x1
     dy = y2 - y1
     steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
+    steps = steps if steps > 0 else 1
     xinc = dx / float(steps)
     yinc = dy / float(steps)
-    for i in range(steps):
+    for i in range(round(steps)):
         x1 += xinc
         y1 += yinc
-        canvas.image.put('black', (round(x1)-pixel_size, round(y1)-pixel_size, round(x1)+pixel_size, round(y1)+pixel_size))
-
+        canvas.create_rectangle(round(x1)-pixel_size, round(y1)-pixel_size, round(x1)+pixel_size, round(y1)+pixel_size, fill='black', outline='')
 def rectDraw(x1, y1, x2, y2, pixel_size):
     ddaLine(x1, y2, x2, y2, pixel_size)
     ddaLine(x2, y2, x2, y1, pixel_size)
@@ -49,7 +79,7 @@ canvas.pack()
 image = PhotoImage(width=600, height=600)
 canvas.image = image
 canvas.create_image((300, 300), image=image, state="normal")
-canvas.bind('<Button-1>', draw_line)
+canvas.bind('<Button-1>', draw_polygon)
 
 click_num = 0
 
